99cf4ab0d907d360d612e2137f31033a0a93c9ee
Daniel O'Brien <emaildano@yahoo.com>
2018-10-23 17:20:30 -0600

    Cosmetic changes made to modal
    This is the first commit made on this branch.

---
+++ PermitDetail.vue
@@ -0,0 +1,122 @@
+<template>
+  <div v-if="visible === true">
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <div class="modal-card-head">
+          <p class="modal-class-title">Permit Detail<input class="input" v-model="permit.address" readonly/> </p>
+        </div>
+        <div class="modal-card-body">
+          <div class="columns">
+            <div class="column is-half">
+              <div class="field is-expanded">
+                <div class="field has-addons">
+                  <p class="control">
+                    <a class="button is-static">
+                      Building type
+                    </a>
+                  </p>
+                  <p class="control is-expanded">
+                    <input class="input" v-model="permit.usesAndscopes" readonly/>
+                  </p>
+                </div>
+              </div>
+              <div class="field is-expanded">
+                <div class="field has-addons">
+                  <p class="control">
+                    <a class="button is-static">
+                      Permit(s)
+                    </a>
+                  </p>
+                  <p class="control is-expanded">
+                    <input class="input" v-model="permit.permitTypes" readonly/>
+                  </p>
+                </div>
+              </div>
+              <div class="field is-expanded">
+                <div class="field has-addons">
+                  <p class="control">
+                    <a class="button is-static">
+                      Last Name
+                    </a>
+                  </p>
+                  <p class="control is-expanded">
+                    <input class="input" v-model="permit.primaryLast" readonly/>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="column is-half">
+              <div class="field is-expanded">
+                <div class="field has-addons">
+                  <p class="control">
+                    <a class="button is-static">
+                      Company
+                    </a>
+                  </p>
+                  <p class="control is-expanded">
+                    <input class="input" v-model="permit.primaryCompany" readonly/>
+                  </p>
+                </div>
+              </div>
+              <div class="field is-expanded">
+                <div class="field has-addons">
+                  <p class="control">
+                    <a class="button is-static">
+                      Permit Status
+                    </a>
+                  </p>
+                  <p class="control is-expanded">
+                    <input class="input" v-model="permit.status" readonly/>
+                  </p>
+                </div>
+              </div>
+              <div class="field is-expanded">
+                <div class="field has-addons">
+                  <p class="control">
+                    <a class="button is-static">
+                      Category
+                    </a>
+                  </p>
+                  <p class="control is-expanded">
+                    <input class="input" v-model="permit.category" readonly/>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-card-footer">
+          <p>Buttons here</p>
+        </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="close"></button>
+    </div>
+  </div>
+</template>
+<script>
+  import EventBus from '../util/EventBus'
+
+  export default {
+    name: 'PermitDetail',
+    data: function(){
+      return{
+        permit: null,
+        visible: false,
+      }
+    },
+    created: function(){
+      var that = this
+      EventBus.$on('SHOW_PERMIT_DETAIL', (detail)=>{
+        that.permit = detail;
+        that.visible = true
+      })
+    },
+    methods: {
+      close : function(){
+        this.permit = null
+        this.visible = false
+      }
+    }
+  }
+</script>
---
+++ Permits.vue
@@ -0,0 +1,245 @@
+<template>
+  <div v-if="visible === true">
+    <a @click="showCustomizePanel">
+      <span class="icon is-small">
+        <icon name="chevron-down"></icon>
+      </span>
+    </a>
+    <div id="permits" class="columns">
+        <transition name="fade">
+          <div class="column is-one-fifth" v-if="showCustomize === true">
+            <div id="filtersPanel" class="panel" >
+              <p class="panel-heading">
+                Fields
+              </p>
+              <p class="panel-tabs">
+                <a>Sort</a>
+              </p>
+              <div class="panel-block" v-for="si in sortInfo">
+                {{si.label}}
+                <div class="control">
+                  <label class="radio">
+                    <div class="control">
+                      <div class="radio">
+                        asc.
+                        <input type="radio" v-bind:name="si.name" ascending="true" @click="changeSortDirection">
+                          <span class="icon is-small">
+                            <icon name="sort-alpha-asc"></icon>
+                          </span>
+                        </input>
+                        desc.
+                        <input type="radio" v-bind:name="si.name" ascending="false" @click="changeSortDirection">
+                          <span class="icon is-small">
+                            <icon name="sort-alpha-desc"></icon>
+                          </span>
+                        </input>
+                      </div>
+                    </div>
+                  </label>
+                </div>
+              </div>
+              <div class="panel-block">
+                <a class="button is-primary" @click="closeCustomizePanel">Close</a>
+                <a class="button is-success" @click="applyNewFilterInfo">Apply</a>
+              </div>
+            </div>
+          </div>
+        </transition>
+        <div class="column">
+        <!-- HERE IS A TOGGLE FOR THE PERMIT BEING APPROVED v-if="showCustomize === true" -->
+          <div class="columns">
+            <div  class="column" v-for="permit in permits">
+              <div class="card">
+                <div class="card-header">
+                  <p class="card-header-title">
+                    {{permit.address}}
+                    <!-- <span v-for="type in permit.permitTypes">{{type}} </span> -->
+                  </p>
+                </div>
+                <div class="card-content">
+                  {{permit.primaryCompany}}
+                  <p>
+                  {{permit.description.slice(0,100)}}
+                  </p>
+                  <p>
+                  ${{permit.totalValue}} total value<br/>
+                  </p>
+                </div>
+                <div class="card-footer has-text-centered">
+                  <a v-on:click="displayPermit">
+                  <input type="hidden" v-bind:value="permit.number" />
+                  Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <div class="level">
+      <div class="level-left">
+        <div class="level-item">
+          <a class="button is-primary" @click="loadPrevious">Previous</a>
+        </div>
+      </div>
+      <div class="level-right">
+        <div class="level-item">
+          <a class="button is-primary" @click="loadNext">Next</a>
+        </div>
+      </div>
+    </div>
+    <permitdetail></permitdetail>
+  </div>
+</template>
+<script>
+  import EventBus from '../util/EventBus'
+  import PermitDetail from './PermitDetail.vue'
+  var apiCall = require("../util/APIcall.js")
+  var $ = require('jquery')
+  var getPermitsPath = 'http://18.232.157.109:8081/api/client/city?area=boulder&report=master&pageSize='
+  var postUserSortInfo = 'http://18.232.157.109:8081/api/client/filters?area=boulder'
+
+  export default {
+    name: 'Permits',
+    components: {
+      'permitdetail': PermitDetail
+    },
+    data: function(){
+      return{
+        permits: [],
+        sortInfo: [],
+        userSortInfo: [],
+        pageSize: 9,
+        page:  0,
+        visible: false,
+        showCustomize : false,
+        token: null
+      }
+    },
+    created: function() {
+      var that = this
+      EventBus.$on('USER_TOKEN_AVAILABLE', (token)=>{
+        that.token = token
+        that.getPermits()
+      })
+      EventBus.$on('SHOW_PERMITS', ()=>{
+        EventBus.$emit('USER_TOKEN_NEEDED')
+        that.visible = true
+      })
+      EventBus.$on('HIDE_ALL', ()=>{
+        that.visible = false
+      })
+    },
+    methods: {
+      getPermits: function () {
+        var that = this
+        apiCall.APIget(getPermitsPath + that.pageSize + '&page=' + this.page , that.token).done(function(response){
+          that.permits = response
+        }).fail(function(){
+          EventBus.$emit('USER_TOKEN_NEEDED')
+        })
+      },
+      displayPermit: function(permitNumber){
+        var permit = this.permits.find(function(permit){
+          if (permit.number === permitNumber.target.children[0].getAttribute("value")){
+            return permit
+          }
+        })
+        EventBus.$emit('SHOW_PERMIT_DETAIL', permit)
+      },
+      loadNext : function(){
+        var that = this
+        this.page += 1
+        apiCall.APIget(getPermitsPath + that.pageSize + '&page=' + this.page , that.token).done(function(response){
+          that.permits = response
+        }).fail(function(){
+          EventBus.$emit('USER_TOKEN_NEEDED')
+        })
+      },
+      loadPrevious : function(){
+        var that = this
+        this.page -= 1
+        if (this.page >= 0){
+          apiCall.APIget(getPermitsPath + that.pageSize + '&page=' + this.page , that.token).done(function(response){
+            that.permits = response
+          }).fail(function(){
+            EventBus.$emit('USER_TOKEN_NEEDED')
+          })
+        }
+        else{
+          this.page = 0
+        }
+      },
+      showCustomizePanel : function(){
+         this.showCustomize = true
+         var address = new Object()
+         address.label = "Address"
+         address.name = "address"
+         address.direction = false
+         this.sortInfo.push(address)
+      },
+      closeCustomizePanel : function(){
+         this.showCustomize = false
+         this.sortInfo = []
+      },
+      changeSortDirection : function (target){
+        var key = target.target.getAttribute('name')
+        var si = this.userSortInfo.find(function(element){
+          return element.field === key
+        })
+        if (si === undefined){
+          si = new Object()
+          si.field = key;
+          this.userSortInfo.push(si)
+        }
+        else{
+          si.ascending = target.target.getAttribute('ascending')
+        }
+      },
+      applyNewFilterInfo: function (){
+        var that = this
+        apiCall.APIput(postUserSortInfo, this.token, this.userSortInfo).done(function(response){
+          that.showCustomize = false
+          this.sortInfo = []
+        }).fail(function(response){
+          //todo pop error
+        })
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  #permits .column {
+      flex-basis: 33%;
+  }
+
+  #permits .panel .panel-block>.control{
+    width: auto;
+    text-align: right;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+
+  @media (max-device-width: 1366px){
+    body{
+      font-size: 0.70rem;
+    }
+  }
+
+  @media (max-device-width: 1024px){
+    body{
+      font-size: 0.55rem;
+    }
+  }
+
+  @media (max-device-width: 824px){
+    body{
+      font-size: 0.35rem;
+    }
+  }
+</style>
